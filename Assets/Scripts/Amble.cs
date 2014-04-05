fd3ad4ceae568b9bbf93ba3f95e0c8ede14798db
P. Autio <Archbang85@gmail.com>
2014-04-05 21:41:18 +0100

    eyeball and fly spawning

---
+++ Assets/Scripts/Amble.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Amble : MonoBehaviour {
+
+	public float minX = -5.0f;
+	public float minZ = -5.0f;
+	public float maxX = 5.0f;
+	public float maxZ = 5.0f;
+
+	private float counter = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		counter -= Time.deltaTime;
+		//Debug.Log (counter);
+		if (counter < 0)
+		{
+
+			float xDir = Random.Range (-10,11);
+			float zDir = Random.Range (-10,11);
+
+			if (this.transform.position.x < minX)
+			{
+				xDir = Mathf.Abs(xDir);
+			}
+			else if (this.transform.position.x > maxX)
+			{
+				xDir = -Mathf.Abs (xDir);
+			}
+			if (this.transform.position.z < minZ)
+			{
+				zDir = Mathf.Abs(zDir);
+			}
+			else if (this.transform.position.z > maxZ)
+			{
+				zDir = -Mathf.Abs (zDir);
+			}
+
+			Vector3 dir = new Vector3(xDir, 0, zDir);
+			float power = Random.Range (1,30) / 10;
+			transform.rigidbody.AddForce(dir.normalized * power, ForceMode.Impulse);
+			//Debug.Log ("pushing to " + dir.normalized); 
+			//Debug.Log ("power " + power); 
+			counter = Random.Range (2,12);
+		}
+
+	
+	}
+}
---
+++ Assets/Scripts/FlyGenerator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyGenerator : MonoBehaviour {
+
+	public float radiusX = 25.0f;
+	public float radiusZ = 12.0f;
+
+	public float innerSpaceRadius = 5.0f;
+
+	public int amount = 12;
+	public GameObject fly;
+
+	// Use this for initialization
+	void Start () {
+
+		for(int i = 0; i < amount; i++)
+		{
+			GameObject flyUnit;
+
+			float xPos = Random.Range (-radiusX, radiusX + 1);
+			float zPos = Random.Range (-radiusZ, radiusZ + 1);
+			while (xPos > -innerSpaceRadius && xPos < innerSpaceRadius && zPos > -innerSpaceRadius && zPos < innerSpaceRadius)
+			{
+				xPos = Random.Range (-radiusX, radiusX + 1); // reroll so it doesn't go too close to the start point
+				zPos = Random.Range (-radiusZ, radiusZ + 1); // reroll so it doesn't go too close to the start point
+			}
+
+			Vector3 spawnPosition = new Vector3(xPos, 5, zPos);
+			flyUnit = Instantiate (fly, spawnPosition, Quaternion.identity) as GameObject;
+			flyUnit.transform.Rotate (90,Random.Range(0, 360),0,Space.World);
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
